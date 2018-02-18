8806df1a338bb63bfd1da23d279f2b262634f640
Andrew Breckenridge <asbreckenridge@me.com>
2018-02-18 05:18:48 -0800

    🚀

---
+++ libMoji/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  libMoji
+//
+//  Created by Andrew Breckenridge on 2/18/18.
+//  Copyright © 2018 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+import SwiftSyntax
+
+print("Hello, World!")
+
